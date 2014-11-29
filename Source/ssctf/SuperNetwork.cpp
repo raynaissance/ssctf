696aaeba2b2018b15c1949094daa2a4befb1b198
Akshay Shanbhogue <shanbhog@usc.edu>
2014-11-28 17:31:35 -0800

    + menuHUD2.uasset calls connect on begin play

--- Source/ssctf/SuperNetwork.cpp
+++ Source/ssctf/SuperNetwork.cpp
@@ -1,20 +1,66 @@
-
-
 #include "ssctf.h"
 #include "SuperNetwork.h"
 #include "easywsclient.hpp"
+//#include <string.h>
+#include <iostream>
+#include <deque>
+
+#include "AllowWindowsPlatformTypes.h"
+#include <thread>
+#include "HideWindowsPlatformTypes.h"
+
+using namespace std;
+using easywsclient::WebSocket;
+
+void handle_message(const std::string & message) {
+	cout << "received " << message.c_str() << endl;
+	//if (message == "world") { ws->close(); }
+}
+
+// TODO is static reqd?
+deque<string> messageQueue;
+
+void workerFunction(){
+	cout << "Worker running" << endl;
+
+	WebSocket::pointer ws = WebSocket::from_url("ws://localhost:1338");
+	//assert(ws);
+	cout << "web socket opened";
+	while (ws->getReadyState() != WebSocket::CLOSED) {
+		if (messageQueue.empty())
+			continue;
+		string message = messageQueue.front();
+		cout << "sending " << message << endl;
+		ws->send(message);
+		ws->poll();
+		ws->dispatch(handle_message);
+		messageQueue.pop_front();
+	}
+	delete ws;
+
+	cout << "Worker done" << endl;
+}
 
 USuperNetwork::USuperNetwork(const class FPostConstructInitializeProperties& PCIP)
 	: Super(PCIP)
 {
-
+	
 }
 
 
-//Happy Message
-FString USuperNetwork::GetHappyMessage()
+void USuperNetwork::Connect()
 {
-	return FString("Victory! Victory BP Library Works!");
+	cout << "Start" << endl;
+
+	thread workerThread(workerFunction);
+	cout << "Waiting for thread" << endl;
+
+	for (int i = 0; i < 100; i++)
+		messageQueue.push_back("p" + to_string(i));
+
+	workerThread.detach();
+
+	cout << "Main done" << endl;
 }
 
 
--- Source/ssctf/SuperNetwork.h
+++ Source/ssctf/SuperNetwork.h
@@ -14,8 +14,8 @@ class SSCTF_API USuperNetwork : public UBlueprintFunctionLibrary
 	GENERATED_UCLASS_BODY()
 
 	UFUNCTION(BlueprintCallable, Category = "SuperNetwork")
-	static FString GetHappyMessage();
+	static void Connect();
 
 	UFUNCTION(BlueprintCallable, Category = "SuperNetwork")
-		static void Register();
+	static void Register();
 };
